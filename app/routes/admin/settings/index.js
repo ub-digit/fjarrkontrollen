b7fd2d8fa8798a8515a237b90ce8867a71c0368d
David Gustafsson <david.gustafsson@ub.gu.se>
2023-11-27 16:49:00 +0100

    Various fixes and temporarily disable user info

--- app/routes/admin/settings/index.js
+++ app/routes/admin/settings/index.js
@@ -1,13 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import RSVP from 'rsvp';
-import { inject } from '@ember/service';
 import ResetScroll from "../../../mixins/reset-scroll";
 
-export default Route.extend(ResetScroll, {
-  session: inject('session'),
+export default class SettingsRoute extends Route.extend(ResetScroll) {
+  @service session;
 
   beforeModel(/* transition */) {
     this.transitionTo('admin.settings.templates');
   }
-  
-});
+}
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
-export default class ApplicatoinRoute extends Route {
+export default class ApplicationRoute extends Route {
   @service session;
   @service sessionAccount;
 
@@ -24,13 +25,12 @@ export default class ApplicatoinRoute extends Route {
       .catch(() => this.session.invalidate());
   }
 
-  actions = {
-    loading(transition, originRoute) {
-      let controller = this.controllerFor('application');
-      controller.set('isLoading', true);
-      transition.promise.finally(function() {
-        controller.set('isLoading', false);
-      });
-    }
+  @action
+  loading(transition, originRoute) {
+    let controller = this.controllerFor('application');
+    controller.set('isLoading', true);
+    transition.promise.finally(function() {
+      controller.set('isLoading', false);
+    });
   }
 }
